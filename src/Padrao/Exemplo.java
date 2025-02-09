211b9cb9242ff55b95d145d51ac9bbf677a3c1b7
Caio Sabino <caiomsabino@gmail.com>
2025-02-08 23:56:35 -0300

    adicao classe exemplo e comentario

---
+++ src/Padrao/Exemplo.java
@@ -0,0 +1,8 @@
+package Padrao;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        String mensagem = "Hello, World!";
+        System.out.println(mensagem);
+    }
+}
--- src/Padrao/MainVetores.java
+++ src/Padrao/MainVetores.java
@@ -24,7 +24,7 @@ public class MainVetores {
         return Integer.parseInt(JOptionPane.showInputDialog(null, mensagem));
     }
 }
-
+//oi
 class Vetor {
     private int[] vetor;
 
